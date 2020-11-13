ac8c780380d114bc556a7b994d4dff56c185fb7d
Ashley Anness <ashleyaanness@gmail.com>
2020-11-12 21:04:19 -0500

    updated employee lists to render

--- db/employeeTracker.sql
+++ db/employeeTracker.sql
@@ -4,24 +4,28 @@ CREATE DATABASE employeeTracker;
 USE employeeTracker;
 
 CREATE TABLE department (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  d_name VARCHAR(30)
+  id INT AUTO_INCREMENT,
+  d_name VARCHAR(30),
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
-  INDEX dep_ind (department_id),
+  PRIMARY KEY(id),
+  FOREIGN KEY (department_id) REFERENCES department(department_id)
+  
 );
 
 CREATE TABLE employee (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  INDEX role_ind (role_id),
-  manager_id INT,
-  INDEX man_ind (manager_id),
-);
+  manager_id INT ,
+  PRIMARY KEY(id),
+  FOREIGN KEY(role_id) REFERENCES role(role_id),
+  FOREIGN KEY(manager_id) REFERENCES department(manager_id)
+)
--- server.js
+++ server.js
@@ -57,7 +57,8 @@ function runSearch() {
                 case "View all employees":
 
                     byEmployees();
-                    runSearch();
+                    
+                    //inquirer.prompt if they want to do somehting aagain  if yes then runSearch()
 
                     break;
                 // Start new case
@@ -255,12 +256,12 @@ function runSearch() {
 // "View all employees",
 function byEmployees() {
 
-    var results = connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.d_name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;",
-
-
-        function (error, results) {
+    //var results = connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.d_name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;",
+        console.log('byEmployees function has fired')
+       connection.query('SELECT * FROM employee', function(error, results) {
             if (error) throw error
             console.table(results)
+            return
         })
 
 };
